a27e315204a538e2f7744f407ac40a3262b85f1c
Andy Wong <andykswong@outlook.com>
2021-09-03 21:06:22 -0700

    Fix build script

--- scripts/lnk.mjs
+++ scripts/lnk.mjs
@@ -1,4 +1,7 @@
 import lnk from 'lnk';
 
 lnk(['src/common'], 'assembly', { force: true, type: 'symbolic' })
-    .then(() => console.log('Created synlink from assembly/common to src/common'));
+  .then(
+    () => console.log('Created symlink from assembly/common to src/common'),
+    (e) => console.warn('Failed to create symlink: ' + e)  
+  );
